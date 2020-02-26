17ece7490da7364e9479eacd46efa4ebb0b85298
vguillemette <vincent.guillemette@dti.ulaval.ca>
2020-02-26 11:59:07 -0500

    * express

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
         '@typescript-eslint/no-inferrable-types': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-empty-function': 'off',
-        '@typescript-eslint/no-empty-interface': 'off'
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-var-requires': 'off'
     },
     overrides: [
         {
---
+++ server.js
@@ -0,0 +1,49 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+// const publicationApi = require('./server/api/publication-api');
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const MongoStore = require('connect-mongo')(expressSession);
+const sessionMiddleWare = expressSession({
+    secret: 'test',
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+});
+
+require('./server/passport/serializer');
+require('./server/passport/local-strategy');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(sessionMiddleWare);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'dist/dti-forum')));
+
+// app.use('/api/publications', publicationApi);
+
+app.get('*', (_, res) =>
+    res.sendFile(path.join(__dirname, 'dist/study/index.html'))
+);
+
+server.listen(process.env.PORT, () =>
+    console.log(
+        `Dti-Overflow is running and listening on port ${process.env.PORT}`
+    )
+);
